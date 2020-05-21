451fefdc703cec94bb15b5caa5054d042e28a6bb
yaozefeng <1343047957@qq.com>
2020-05-21 18:28:01 +0800

    "项目提交5.21"

---
+++ guigumall-member/src/main/java/com/atguigu/gulimall/member/GuigumallMemberApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuigumallMemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuigumallMemberApplication.class, args);
+	}
+
+}
